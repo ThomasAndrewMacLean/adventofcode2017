8bae82dd2f97d676d68cd065044817bd1db0cc0c
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2017-12-02 17:36:36 +0100

    day1 ok

---
+++ day1/CaptchaSolver.js
@@ -0,0 +1,47 @@
+class CaptchaSolver {
+    constructor(inputString) {
+        this.inputString = inputString;
+    }
+
+    lengteIs() {
+        return this.inputString.length;
+    }
+
+
+    loopOver() {
+        let result = 0;
+        this.counter = 0;
+        const plus = 1;
+        for (let letter of this.inputString) {
+
+            if (letter === this.inputString[(this.counter + plus) % this.lengteIs()]) {
+
+                result += parseInt(letter);
+            }
+            this.counter++;
+
+        }
+        return result;
+
+    }
+
+
+    loopOver2() {
+        let result = 0;
+        this.counter = 0;
+        const plus = this.lengteIs()/2;
+        for (let letter of this.inputString) {
+
+            if (letter === this.inputString[(this.counter + plus) % this.lengteIs()]) {
+
+                result += parseInt(letter);
+            }
+            this.counter++;
+
+        }
+        return result;
+
+    }
+
+}
+module.exports = CaptchaSolver;
--- day1/day1a.js
+++ day1/day1a.js
@@ -1,4 +1,10 @@
 console.log('hellow world!!');
 var fs = require('fs');
-const cont = fs.readFileSync('input1a.txt', 'utf8');
-console.log(cont);
+var cs = require('./CaptchaSolver.js');
+const cont = fs.readFileSync('day1/input1a.txt', 'utf8');
+// console.log(cont);
+
+var cs = new cs(cont);
+
+var antwoord = cs.loopOver2();
+console.log(antwoord);
