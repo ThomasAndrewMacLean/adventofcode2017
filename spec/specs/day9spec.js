2ccd21b715ecdb84670f614cd2194c750d3edf43
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2017-12-12 11:49:36 +0100

    day9

--- spec/specs/day9spec.js
+++ spec/specs/day9spec.js
@@ -1,5 +1,21 @@
 'use strict';
 
+let inp =
+    `{{<ab>},{<ab>},{<ab>},{<ab>}}
+{{<!!>},{<!!>},{<!!>},{<!!>}}
+{{<a!>},{<a!>},{<a!>},{<ab>}}
+{{<!!>},{<!!>},{<!!>},{<!!!>}}`;
+let demo = inp.split('\n');
+let inpB = `<>
+<random characters>
+<<<<>
+<{!>}>
+<!!>
+<!!!>>
+<{o"i!a,<{i<a>`;
+let demoB = inpB.split('\n');
+
+
 describe('Day9', function () {
     let Day9 = require('../../src/day9/day9.js');
     let day9;
@@ -11,4 +27,49 @@ describe('Day9', function () {
     it('day9 should create', function () {
         expect(day9).toBeTruthy();
     });
+    it('day9 remove cancellations', function () {
+        expect(day9.removeCancel(demo[1])).toBe('{{<>},{<>},{<>},{<>}}');
+        expect(day9.removeCancel(demo[3])).toBe('{{<>},{<>},{<>},{<}}');
+    });
+    it('day9 remove cancellations', function () {
+        expect(day9.removeCancel(demo[0])).toBe('{{<ab>},{<ab>},{<ab>},{<ab>}}');
+    });
+    it('day9 remove garbage', function () {
+        expect(day9.removeGarbage(demo[0])).toBe('{{},{},{},{}}');
+    });
+    it('day9 remove cancel then garbage', function () {
+        expect(day9.removeGarbage(day9.removeCancel(demo[2]))).toBe('{{}}');
+    });
+    it('day9 get points demo0 is 9', function () {
+        expect(day9.exA(demo[0])).toBe(9);
+    });
+    it('day9 get points demo1 is 9', function () {
+        expect(day9.exA(demo[1])).toBe(9);
+    });
+    it('day9 get points demo2 is 3', function () {
+        expect(day9.exA(demo[2])).toBe(3);
+    });
+
+    it('day9 countGarbase demoB0 is 0', function () {
+        expect(day9.exB(demoB[0])).toBe(0);
+    });
+    it('day9 countGarbase demoB1 is 17', function () {
+        expect(day9.exB(demoB[1])).toBe(17);
+    });
+    it('day9 countGarbase demoB2 is 3', function () {
+        expect(day9.exB(demoB[2])).toBe(3);
+    });
+    it('day9 countGarbase demoB3 is 2', function () {
+        expect(day9.exB(demoB[3])).toBe(2);
+    });
+    it('day9 countGarbase demoB4 is 0', function () {
+        expect(day9.exB(demoB[4])).toBe(0);
+    });
+    it('day9 countGarbase demoB5 is 0', function () {
+        expect(day9.exB(demoB[5])).toBe(0);
+    });
+    it('day9 countGarbase demoB6 is 10', function () {
+        expect(day9.exB(demoB[6])).toBe(10);
+    });
+
 });
--- src/day9/day9.js
+++ src/day9/day9.js
@@ -3,11 +3,52 @@ const inputPuzzle = fs.readFileSync('src/day9/input9.txt', 'utf8');
 
 class Day9 {
 
+    removeCancel(inp) {
+        return inp.split(/!./g).join('');
+    }
+    removeGarbage(inp) {
+        return inp.split(/<[^>]*>/g).join('');
+    }
+    getScore(inp) {
+        let score = 0;
+        let point = 0;
+
+        for (let c of inp) {
+            if (c === '{') {
+                point++;
+                score += point;
+            }
+            if (c === '}') {
+                point--;
+            }
+        }
+        return score;
+    }
+    countGarbage(inp) {
+        let x = inp.match(/<[^>]*>/g);
+        let counter = x.length;
+        let y = x.join('');
+        let z = y.length;
+
+        return z - counter * 2;
+    }
 
 
-
-
+    exA(inp) {
+        inp = this.removeCancel(inp);
+        inp = this.removeGarbage(inp);
+        return this.getScore(inp);
+    }
+    exB(inp) {
+        inp = this.removeCancel(inp);
+        return this.countGarbage(inp);
+    }
 }
 
 
+let d = new Day9();
+
+let res = d.exB(inputPuzzle);
+
+console.log(res);
 module.exports = Day9;
