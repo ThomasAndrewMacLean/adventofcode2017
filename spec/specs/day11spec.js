76209777d63881cea9d49860447376df2bfb37b6
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2017-12-14 13:29:55 +0100

    day11a

--- spec/specs/day11spec.js
+++ spec/specs/day11spec.js
@@ -15,4 +15,27 @@ describe('Day11', function () {
     it('day11 should create', function () {
         expect(day11).toBeTruthy();
     });
+
+    it('day11 counter get highest', function () {
+        day11.counter = [1, 3, 5];
+        expect(day11.getDistance()).toBe(5);
+    });
+    it('day11 do steps', function () {
+        day11.doStep('n');
+        expect(day11.counter).toEqual([1, 0, 0]);
+    });
+    it('day11 exA 1', function () {
+        expect(day11.exA('ne,ne,ne')).toEqual(3);
+    });
+    it('day11 exA 2', function () {
+        expect(day11.exA('ne,ne,sw,sw')).toEqual(0);
+    });
+    it('day11 exA 3', function () {
+        expect(day11.exA('ne,ne,s,s')).toEqual(2);
+    });
+    it('day11 exA 4', function () {
+        expect(day11.exA('se,sw,se,sw,sw')).toEqual(3);
+    });
+
+
 });
--- src/day11/day11.js
+++ src/day11/day11.js
@@ -3,6 +3,54 @@
 const fs = require('fs');
 const inputPuzzle = fs.readFileSync('src/day11/input11.txt', 'utf8');
 
-class Day11 {}
+class Day11 {
+    constructor() {
+        this.counter = [0, 0, 0];
+    }
+
+    getDistance() {
+        return Math.max(...this.counter.map(c => Math.abs(c)));
+    }
+
+    doStep(step) {
+        switch (step) {
+        case 'n':
+            this.counter[0]++;
+            break;
+        case 'ne':
+            this.counter[1]++;
+            break;
+        case 'se':
+            this.counter[2]++;
+            break;
+        case 's':
+            this.counter[0]--;
+            break;
+        case 'sw':
+            this.counter[1]--;
+            break;
+        case 'nw':
+            this.counter[2]--;
+            break;
+
+        default:
+            break;
+        }
+
+    }
+    exA(inp) {
+        let steps = inp.split(',');
+
+        for (let step of steps) {
+            this.doStep(step);
+        }
+
+        return this.getDistance();
+
+    }
+
+
+
+}
 
 module.exports = Day11;
